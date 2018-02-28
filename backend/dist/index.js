ddc92040b7721de9cbe160c48421a846ac29bc9d
shevchenkobn <bohdan.shevchenko1@nure.ua>
2018-02-28 11:03:57 +0200

    configured environment

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,9 +1,21 @@
-var Colors;
-(function (Colors) {
-    Colors[Colors["Red"] = 0] = "Red";
-    Colors[Colors["Green"] = 1] = "Green";
-    Colors[Colors["Blue"] = 2] = "Blue";
-})(Colors || (Colors = {}));
-;
-console.log(Colors.Blue);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+var hapi_1 = __importDefault(require("hapi"));
+var server = new hapi_1.default.Server({
+    port: 3000,
+    host: "127.0.0.1"
+});
+server.start().then(function () {
+    console.log(server.info.uri);
+});
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply.response("Hello, " + encodeURIComponent(request.params.name));
+    }
+});
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,16 @@
-import hapi from "hapi";
+import Hapi from "hapi";
+
+const server = new Hapi.Server({
+  port: 3000,
+  host: "127.0.0.1"
+});
+server.start().then(() => {
+  console.log(server.info.uri);
+});
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, reply) => {
+    reply.response(`Hello, ${encodeURIComponent(request.params.name)}`);
+  }
+});
