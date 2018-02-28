0e87f025d96dadd9351539a54adafacfa975e09d
shevchenkobn <bohdan.shevchenko1@nure.ua>
2018-02-28 10:28:40 +0200

    initial back commit

---
+++ backend/dist/index.js
@@ -0,0 +1,9 @@
+var Colors;
+(function (Colors) {
+    Colors[Colors["Red"] = 0] = "Red";
+    Colors[Colors["Green"] = 1] = "Green";
+    Colors[Colors["Blue"] = 2] = "Blue";
+})(Colors || (Colors = {}));
+;
+console.log(Colors.Blue);
+//# sourceMappingURL=index.js.map
---
+++ backend/src/index.ts
@@ -0,0 +1,7 @@
+enum Colors {
+  Red,
+  Green,
+  Blue
+};
+
+console.log(Colors.Blue);
