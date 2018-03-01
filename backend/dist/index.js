ac6c76c8653a1410a80db6fd57e766edabd5046a
shevchenkobn <bohdan.shevchenko1@nure.ua>
2018-03-01 07:01:33 +0200

    tut

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,19 +3,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-var hapi_1 = __importDefault(require("hapi"));
-var server = new hapi_1.default.Server({
-    port: 3000,
-    host: "127.0.0.1"
+const koa_1 = __importDefault(require("koa"));
+const koa_router_1 = __importDefault(require("koa-router"));
+const app = new koa_1.default();
+const router = new koa_router_1.default();
+const get = (ctx, next) => {
+    ctx.body = "Hello, World";
+};
+const post = (ctx, next) => {
+    ctx.body = "Post hello";
+};
+router.get('/:name/:id([0-9]{0,5})', (ctx) => {
+    ctx.body = ctx.params.name + ' with ' + ctx.params.id;
 });
-server.start().then(function () {
-    console.log(server.info.uri);
+router.get('/hello', get);
+router.post('/hello', post);
+router.all('/test', (ctx, next) => {
+    ctx.body = "all test";
 });
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply.response("Hello, " + encodeURIComponent(request.params.name));
-    }
+app.use(router.routes())
+    .listen(3000, () => {
+    console.log('listening');
 });
 //# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,27 @@
-import Hapi from "hapi";
+"use strict";
+import KoaApplication from 'koa';
+import KoaRouter, {IMiddleware} from 'koa-router';
 
-const server = new Hapi.Server({
-  port: 3000,
-  host: "127.0.0.1"
+const app = new KoaApplication();
+const router = new KoaRouter();
+
+const get: IMiddleware = (ctx, next) => {
+  ctx.body = "Hello, World";
+};
+const post: IMiddleware = (ctx, next) => {
+  ctx.body = "Post hello";
+};
+
+router.get('/:name/:id([0-9]{0,5})', (ctx) => {
+  ctx.body = ctx.params.name + ' with ' + ctx.params.id;
 });
-server.start().then(() => {
-  console.log(server.info.uri);
+router.get('/hello', get);
+router.post('/hello', post);
+router.all('/test', (ctx, next) => {
+  ctx.body = "all test";
 });
-server.route({
-  method: 'GET',
-  path: '/{name}',
-  handler: (request, reply) => {
-    reply.response(`Hello, ${encodeURIComponent(request.params.name)}`);
-  }
+
+app.use(router.routes())
+.listen(3000, () => {
+  console.log('listening');
 });
