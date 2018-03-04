dea2b9671bc101cc8ba24473db2b5571df9e8486
shevchenkobn <bohdan.shevchenko1@nure.ua>
2018-03-04 16:50:35 +0200

    at least configured fcking workflow

--- backend/bootstrap.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const vm = require('vm');
-
-var mainCode = fs.readFileSync(__dirname + '/dist/index.js');
-vm.runInThisContext(module.constructor.wrap(mainCode), 'file://app/main.js')(exports, require, module, __filename, __dirname, process, global, Buffer);
