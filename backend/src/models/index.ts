f21b51db8e280e02511ebc7a82ac039d4931fc48
shevchenkobn <bohdan.shevchenko1@nure.ua>
2018-05-01 10:12:18 +0300

    checked initialization of board models

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -5,6 +5,17 @@ import CellFunctionInitializer from './cellFunction.model';
 import CellFunctionClassInitializer from './cellFunctionClass.model';
 import mongoose from 'mongoose';
 
+export interface IModelInitializer<IModel extends mongoose.Model<IDocument>, IDocument extends mongoose.Document> {
+  bindToConnection(connection: mongoose.Connection | typeof mongoose, name?: string): IModel;
+  getModel(): IModel;
+  isBoundToConnection(connection?: mongoose.Connection | typeof mongoose): boolean;
+  getModelName(): string;
+}
+
+export interface IModels {
+  [name: string]: mongoose.Model<mongoose.Document>;
+}
+
 export let models: IModels = null;
 export function initialize(connection: mongoose.Connection | typeof mongoose): IModels {
   if (models) {
@@ -18,15 +29,4 @@ export function initialize(connection: mongoose.Connection | typeof mongoose): I
     [CellFunctionClassInitializer.getModelName()]: CellFunctionClassInitializer.bindToConnection(connection)
   };
   return models;
-}
-
-export interface IModelInitializer<IModel extends mongoose.Model<IDocument>, IDocument extends mongoose.Document> {
-  bindToConnection(connection: mongoose.Connection | typeof mongoose, name?: string): IModel;
-  getModel(): IModel;
-  isBoundToConnection(connection?: mongoose.Connection | typeof mongoose): boolean;
-  getModelName(): string;
-}
-
-export interface IModels {
-  [name: string]: mongoose.Model<mongoose.Document>;
 }
