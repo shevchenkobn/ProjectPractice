d34ec41447bdccf08f0e321d3224a47d5e4bd5ca
Taushanov Nikita <mykyta.taushanov@nure.ua>
2018-05-27 00:09:57 +0300

    Simple animation created, PlayerPrefsHelper added

---
+++ unityApps/FeudalMonopoly/Assets/Scripts/Helpers/PlayerPrefsHelper.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerPrefsHelper : MonoBehaviour
+{
+    const string TOKEN_KEY = "token";
+
+    public static void SetToken(string token)
+    {
+        PlayerPrefs.SetString(TOKEN_KEY, token);       
+    }
+
+    public static string GetToken()
+    {
+        return PlayerPrefs.GetString(TOKEN_KEY);
+    }
+}
